c3ecad0708c9010d02344818fb681069ca10867e
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-01 15:49:32 -0700

    argument processing test

--- test/greet_test.js
+++ test/greet_test.js
@@ -2,24 +2,27 @@
 
 var assignment = require('../assignment');
 var expect = require('chai').expect;
+
 var greet = require(__dirname + '/../lib/greet');
 
-describe('assignment', function() {
+process.argv = ['node', 'myprogram.js', 'johnny'];
+//sample input from user
+var greetProgramResult = require('../bin/greet');
+
+describe('greeter', function() {
   it('should return hello + name', function() {
     expect(assignment.greet('test')).to.eql('hello ' + 'test');
   });
 });
 
-describe('assignment', function() {
+describe('greeter', function() {
   it('should return undefined if no parameter is given', function () {
     expect(assignment.greet()).to.eql(undefined);
   });
 });
 
-describe('command line greeting', function() {
-  it('should take the argument and say hello + arg', function() {
-
-    expect(assignment.greet(process.argv[2])).to.eql('hello ' + process.argv[2]);
-
+describe('test the greet program', function() {
+  it('should return hello jonny', function() {
+    expect(greetProgramResult).to.eql('hello johnny');
   });
 });
