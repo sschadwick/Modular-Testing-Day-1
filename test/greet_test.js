d79b7a0b33b715af450e7b5272193fa3597cfe87
“sschadwick” <sschadwick@gmail.comssh-keygen -t rsa -C sschadwick@gmail.com>
2015-09-01 15:53:11 -0700

    argument processing test

--- test/greet_test.js
+++ test/greet_test.js
@@ -4,6 +4,10 @@ var assignment = require('../assignment');
 var chai = require('chai');
 var expect = chai.expect;
 
+process.argv = ['node', 'myprogram.js', 'johnny'];
+//sample input from user
+var greetProgramResult = require('../bin/greet');
+
 describe('assignment', function() {
   it('should return hello + name', function() {
     expect(assignment.greet('Steven')).to.eql('hello ' + 'Steven');
@@ -13,5 +17,11 @@ describe('assignment', function() {
 describe('assignment', function() {
   it('should return undefined if no parameter is given', function () {
     expect(assignment.greet()).to.eql(undefined);
+  });
+});
+
+describe('test the greet program', function() {
+  it('should return hello jonny', function() {
+    expect(greetProgramResult).to.eql('hello johnny');
   });
 });
